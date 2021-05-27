13c098c36741af2486f7bcc0b38245af6ac92988
Deploy from CI <>
2021-05-27 19:05:07 +0000

    Deploy 33c791e1f54c585a002ca244ac8ce2ef01a9cc34 to gh-pages

---
+++ api/implementors/wasmtime_runtime/traphandlers/trait.TrapInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["wasmtime"] = [{"text":"impl TrapInfo for <a class=\"struct\" href=\"wasmtime/struct.Store.html\" title=\"struct wasmtime::Store\">Store</a>","synthetic":false,"types":["wasmtime::store::Store"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
